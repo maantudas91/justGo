72d3071d41567c654ece072729f6256bc670e12b
Maantu Das <maantudas91@gmail.com>
2018-08-27 17:52:27 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,19 +6,23 @@ const userController = require('../controllers/userController');
 const vehicleController = require('../controllers/vehicleController');
 const orderController = require('../controllers/orderController');
 
+//const authenticate  = require('../middleware/authenticate');
+
+
+/**********Admin APIs******** */
+
 
 /* User */
 router.get('/list/user', userController.getUsers);
 router.post('/add/user', userController.addUser);
 router.get('/get/user', userController.getUser);
-router.post('/user/login', userController.login)
 
 
 
 
 /* BU */
 router.get('/list/bu', buController.getBUs);
-router.post('add/bu', buController.addBU);
+router.post('/add/bu', buController.addBU);
 
 /* Vehicle */
 router.get('/list/vehicle', vehicleController.getVehicles);
@@ -28,5 +32,15 @@ router.post('/add/vehicle', vehicleController.addVehicle);
 /* Order */
 router.get('/list/order', orderController.getOrders);
 router.post('/add/order', orderController.newOrder);
+
+
+
+
+
+/******************User APIs************************* */
+
+router.post('/user/signup', userController.signup);
+router.post('/user/login', userController.login);
+router.get('/user/check', userController.authenticate, userController.userCheck);
   
 module.exports = router;
