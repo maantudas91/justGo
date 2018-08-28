9301a06ba4aa568286d4d493cc5e5e0951575112
Maantu Das <maantudas91@gmail.com>
2018-08-28 17:35:42 +0530

    Update buController.js

--- controllers/buController.js
+++ controllers/buController.js
@@ -1,6 +1,7 @@
 const BU = require('./../models/BU');
 const Id = require('valid-objectid');
 const utils = require('./../utils/utils');
+const _ = require('lodash');
 
 
 exports.getBUs = async function(req, res, next) {
@@ -17,6 +18,7 @@ exports.getBUs = async function(req, res, next) {
 
 
 exports.addBU = function(req, res, next) {
+    
     var bu = new BU(req.body);
     bu.save(function (err, bu) {
             if(err){
@@ -25,4 +27,32 @@ exports.addBU = function(req, res, next) {
             }
             return res.status(201).jsonp({'success' : true});
     });
+ }
+
+
+
+ exports.editBU = function(req,res){
+    let buid = req.params.buid;
+    //checking valid objectID
+    if (!Id.isValid(buid)) {
+        return res.status(403).jsonp({'success' : false, msg: 'Something wrong happend.Please try again'});
+    }else{
+        let reqBody = req.body;
+        let checkId  = reqBody.hasOwnProperty('_id') && (reqBody._id == buid);
+
+        if(!checkId){
+            return res.status(403).jsonp({'success' : false, msg: 'Something wrong happend.Please try again'});
+            
+        }else{
+            let buID = reqBody._id;
+            delete reqBody._id;
+            let query = BU.update({ _id: buID}, { $set: reqBody});
+            query.exec(function(err, bu){
+                if(err)
+                    return res.status(403).jsonp({'success' : false, err});
+                return res.status(200).jsonp({'success' : true, bu}); 
+            })
+        }
+        
+    }
  }
