3feda292de3e89cd4b22a753c4a54d4a16cdf51a
Maantu Das <maantudas91@gmail.com>
2018-08-28 17:31:33 +0530

    Update vehicleController.js

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -1,4 +1,5 @@
 const Vehicle = require('./../models/Vehicle');
+const BU = require('./../models/BU');
 const Id = require('valid-objectid');
 const utils = require('./../utils/utils');
 
@@ -27,3 +28,24 @@ exports.addVehicle = function(req, res, next) {
             return res.status(201).jsonp({'success' : true});
     });
  }
+
+
+exports.listVehiclebyBU = function (req,res){
+    let buid = req.params.buid;
+    //buid validate
+    if (!Id.isValid(buid)) {
+        return res.status(403).jsonp({'success' : false, msg: 'Invalid BU id'});
+    }else{
+        let query = BU.findById(buid);
+        query.populate('vehicles', 'name reg_no');
+        query.select('vehicles -_id');
+
+        query.exec(function( err, vehs){
+            if(err)
+                return res.status(403).jsonp({'success' : false});
+            return res.status(200).jsonp({'success' : true, 'result': vehs});   
+        });
+    }
+    
+    
+}
